44ba166560ab039b46c5050bb7eeed71dc52419d
Laksh20 <85684445+Laksh20@users.noreply.github.com>
2021-11-14 16:23:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(10);
+
+}
+
+function KeyIsDown(){
+  if (KeyIsDown(DOWN_ARROW)){
+   background('red');
+ }
+ }
+
+
+
+
